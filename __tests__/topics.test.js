0355e3f9970c0c4109190110360fea13b8371a8a
Deadline76 <dajc76@aol.com>
2023-08-14 12:04:57 +0100

    adds progress on Q2

---
+++ __tests__/topics.test.js
@@ -0,0 +1,20 @@
+const app = require('../app.js');
+const request = require('supertest');
+const db = require('../db/connection.js');
+const seed = require('../db/seeds/seed.js')
+const topicData = require('../db/data/test-data/index.js');
+
+beforeEach(() => seed(topicData));
+afterAll(() => db.end());
+
+describe('api/topics', () => {
+    test.skip('GET: 200, an array of topic objects with properties "slug" and "description"', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((response) => {
+            expect(response.body.topics).toEqual(expect.any(Array));
+            expect.arrayContaining(['slug', 'description']);
+        })
+    })
+})
---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const { getTopics } = require('./controllers/topics-controllers')
+
+app.use(express.json());
+
+
+
+app.get('/api/topics', getTopics);
+
+
+
+
+module.exports = app;
