85868241b87989956b7d9971c55ab53f4974d04d
Deadline76 <dajc76@aol.com>
2023-08-15 12:41:11 +0100

    adds /api/articles/:article_id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,3 +38,39 @@ describe('api/', () => {
         })
     })
 })
+describe('/api/articles/:article_id', () => {
+    test('GET:200 sends a single article to the client', () => {
+      return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then(({body}) => {
+            delete body.msg.created_at
+          expect(body.msg).toMatchObject({
+              article_id: 1,
+              title: "Living in the shadow of a great man",
+              topic: "mitch",
+              author: "butter_bridge",
+              body: "I find this existence challenging",
+              votes: 100,
+              article_img_url:
+                "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+            })
+        })
+    })
+    test('GET:404 sends an appropriate and error message when given a valid but non-existent id', () => {
+        return request(app)
+          .get('/api/articles/999')
+          .expect(404)
+          .then(({body}) => {
+            expect(body.msg).toBe('article does not exist');
+        });
+    })
+    test('GET:400 sends an appropriate and error message when given an invalid id', () => {
+            return request(app)
+              .get('/api/articles/not-an-article')
+              .expect(400)
+              .then(({body}) => {
+                expect(body.msg).toBe('Invalid id');
+        });
+    })
+})
--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
-const { getTopics, getEndpoints } = require('./controllers/topics-controllers.js');
+const { getTopics, getEndpoints, getArticleById } = require('./controllers/controllers.js');
 
 
 app.get('/api', getEndpoints)
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles/:article_id', getArticleById)
+
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404).send({ msg: err.msg })
+    } else {
+        next(err)
+    }
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === 400) {
+        res.status(400).send({ msg: err.msg })
+    } else {
+        next(err)
+    }
+})
 
 app.use((err, req, res, next) => {
     res.status(500).send({ msg: 'Internal Server Error'})
