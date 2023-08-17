dbf6df9a81d276c6596d1d7c9c860e67cf1853ad
Deadline76 <dajc76@aol.com>
2023-08-17 17:12:24 +0100

    removed commented code

--- models/models.js
+++ models/models.js
@@ -45,16 +45,6 @@ const selectAllArticles = (orderBy) => {
     })
   }
 
-// const checkForArticle = (article_id) => {
-//     return db
-//         .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-//         .then(({rows}) => {
-//             if (rows.length === 0) {
-//                 return Promise.reject({status: 404, msg: 'article does not exist'})
-//             }
-//         })
-// }
-
 const insertComment = (username, body, article_id) => {
 
     return db
